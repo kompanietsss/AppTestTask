a5d0b66eb72c4e55a2b380499fa40d4ca476db54
Sergey <sergey@ShopUSs-MacBook-Pro.local>
2020-04-28 10:40:44 +0300

    Project init

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from './src/components/Home';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
